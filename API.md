a220d8bb5fa7c1e3fd2dac597ba3568910335e8e
AliAkbarBadri <aabadri74@gmail.com>
2024-03-11 16:18:04 +0330

    add api base project

---
+++ API.md
@@ -0,0 +1,3 @@
+(.venv) > django-admin startproject django_project .
+(.venv) > python manage.py startapp todos
+(.venv) > python manage.py migrate
--- home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-
-# Create your views here.
-def home(request):
-    # return HttpResponse("home page")
-    person = {"name": "ali", "age": 28}
-    return render(request, "home.html", person)
